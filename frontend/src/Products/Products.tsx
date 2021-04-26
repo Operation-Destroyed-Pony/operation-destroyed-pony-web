2a449cc8a51f9c7bbf919acfb72ea6a0d4664f92
Kris Skugor <skugor.3@osu.edu>
2021-04-26 11:49:30 -0400

    part 1

--- frontend/src/Products/Products.tsx
+++ frontend/src/Products/Products.tsx
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import data from './data';
 
+interface IProduct {
+    imageUrl: string;
+    name: string;
+    brand: string;
+    price: number;
+    rating: number;
+}
+
+const emptyProducts: IProduct[] = [];
+
 function Products() {
+
+    const [products, setProducts]: [
+        IProduct[],
+        (products: IProduct[]) => void
+    ] = useState(emptyProducts);
+
+    useEffect(() => {
+        axios
+            .get<IProduct[]>("https://localhost:5001/catalog", {
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            })
+            .then((response) => setProducts(response.data));
+    }, []);
+
     return (
         <div className="content">
             <ul className="products">
                 {
-                    data.products.map(product => 
+                    products.map((product) => (
                         <li>
                             <div className="product">
                                 <img className="product-image" src={product.imageUrl} alt="product"/>
@@ -16,10 +43,9 @@ function Products() {
                              <div className="product-brand">{product.brand}</div>
                              <div className="product-color">{product.color}</div>
                              <div className="product-price"> ${product.price}</div>
-                             <div className="product-rating">{product.rating} Stars ({product.numberOfReviews} Reviews)</div>
                             </div>
-                        </li>)
-                }
+                        </li>
+                    ))}
             </ul>
         </div>
     );
